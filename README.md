1af1e649005705ece61f5a43a6c08618b393a219
chen <chenshaobo272@gmail.com>
2018-11-26 21:43:19 +0800

    1.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# minQ
+
+> 使用PyQt QWebEngineView写HTML界面,接入有道智云API实现有道词典的查词翻译
---
+++ main.py
@@ -0,0 +1,39 @@
+#!usr/bin/python3
+# -*- coding:utf-8 -*-
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+# from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QTextEdit
+# from FramelessWindow import FramelessWindow
+# from PyQt5.Qt import Qt
+import sys
+
+class MainWindow(QMainWindow):
+	def __init__(self):
+		super().__init__()
+
+		self.initUI()
+
+	def initUI(self):
+		self.setWindowTitle("有道minQ")
+		self.resize(760,520)
+		# self.setFixedSize(760,520)
+		self.center()
+
+		self.page = QWebEngineView(self)
+		url = 'file:///home/chen/PycharmProjects/youdao/dict.html'
+		self.page.load(QUrl(url))
+		self.setCentralWidget(self.page)
+
+
+	def center(self):
+		screen = QDesktopWidget().screenGeometry()
+		size = self.geometry()
+		self.move((screen.width()-size.width())/2,(screen.height()-size.height())/2)
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	win = MainWindow()
+	win.show()
+	sys.exit(app.exec_())
